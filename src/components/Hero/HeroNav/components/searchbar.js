eef9a16bed3bc2283564f8d7518d9dbb4cd0cb8d
Georgeshayek <georgeshayek85@gmail.com>
2024-02-28 17:01:22 +0200

    second commit

--- src/components/Hero/HeroNav/components/searchbar.js
+++ src/components/Hero/HeroNav/components/searchbar.js
@@ -1,22 +1,53 @@
-import { GrFormDown } from "react-icons/gr";
-import { GrFormSearch } from "react-icons/gr";
-import { GrLocation } from "react-icons/gr";
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"; // Import the solid style icon
+import { useState } from "react";
 
-
-
-const SearchBar=()=>{
-    return(
-        <div className="flex bg-white rounded-xl my-4 justify-center mx-auto space-x-8 p-4 w-[80%]">
-            <input className="bg-[#f9f9f9] rounded-xl w-[200px] max-lg:w-[120px] p-2 placeholder-black" placeholder="Job title or keyword"/>
-            
-            <select className="bg-[#f9f9f9] rounded-xl w-[150px] max-lg:w-[100px] p-2">
-
-                <option disabled selected value="">location  </option>
-                <option>contact</option>
-                <option>contact</option>
-            </select>
-            <button className="rounded-xl  bg-[#00cc99] w-[100px]  text-white">Search</button>
-        </div>
-    )
-}
-export default SearchBar
+const SearchBar = () => {
+	const [value, setValue] = useState("");
+	const onChangeHandler = (e) => {
+		setValue(e.target.value);
+	};
+	return (
+		<div className="flex bg-white rounded-xl my-4 justify-center mx-auto space-x-8 p-4 w-[80%]">
+			<input
+				className="bg-[#f9f9f9] rounded-xl w-[200px] max-lg:w-[120px] p-2 placeholder-black"
+				placeholder="&#x1F50E;&#xFE0E;	Job title or keyword"
+			/>
+			{!value && (
+				<div className="bg-[#f9f9f9] rounded-xl w-[150px] max-lg:w-[100px] p-2 flex ">
+					<FontAwesomeIcon
+						icon={faMapMarkerAlt}
+						className="w-[20px] h-[20px]"
+					/>
+					<select onChange={onChangeHandler} className="w-[150px] bg-transparent max-lg:w-[110px]">
+						<option disabled hidden selected value="">
+							
+							location
+						</option>
+						<option value="about">About</option>
+						<option value="contact">contact</option>
+						<option value="small">small</option>
+						<option value="text">text</option>
+					</select>
+				</div>
+			)}
+			{value && (
+				<select
+					className="bg-[#f9f9f9] rounded-xl w-[150px] max-lg:w-[110px] p-2"
+					value={value}
+					onChange={onChangeHandler}
+				>
+					<option value="about">About</option>
+					<option value="contact">contact</option>
+					<option value="small">small</option>
+					<option value="text">text</option>
+				</select>
+			)}
+			<button className="rounded-xl  bg-[#00cc99] w-[100px]  text-white">
+				Search
+			</button>
+		</div>
+	);
+};
+export default SearchBar;
--- src/components/Hero/HeroSign/components/signNav.js
+++ src/components/Hero/HeroSign/components/signNav.js
@@ -1,10 +1,12 @@
 const { default: Link } = require("next/link")
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons"; // Import the solid style icon
 
 const SignNav=()=>{
     return(
-        <div className="flex justify-center space-x-6 my-10 p-2 max-lg:hidden ">
+        <div className="flex justify-center items-center space-x-6 my-10 p-2 max-lg:hidden ">
             <Link href="/" className="font-semibold text-white"> Sign in</Link>
-            <button className=" bg-white rounded font-semibold text-[14px] p-1">create account</button>
+            <button className=" bg-white rounded font-semibold text-[16px] p-1 flex items-center"> <FontAwesomeIcon icon={faSignOutAlt} className="w-[20px] h-[20px] mr-1"/>create account </button>
 
         </div>
     )
