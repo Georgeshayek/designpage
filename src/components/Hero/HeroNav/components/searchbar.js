8660dddfe539a47afedefc5a96dce9ec61f74468
Georgeshayek <georgeshayek85@gmail.com>
2024-02-28 17:07:21 +0200

    css fixes

--- src/components/Hero/HeroNav/components/searchbar.js
+++ src/components/Hero/HeroNav/components/searchbar.js
@@ -9,18 +9,18 @@ const SearchBar = () => {
 		setValue(e.target.value);
 	};
 	return (
-		<div className="flex bg-white rounded-xl my-4 justify-center mx-auto space-x-8 p-4 w-[80%]">
+		<div className="flex bg-white rounded-xl my-4 justify-center mx-auto space-x-8 p-4 w-[90%]">
 			<input
 				className="bg-[#f9f9f9] rounded-xl w-[200px] max-lg:w-[120px] p-2 placeholder-black"
 				placeholder="&#x1F50E;&#xFE0E;	Job title or keyword"
 			/>
 			{!value && (
-				<div className="bg-[#f9f9f9] rounded-xl w-[150px] max-lg:w-[100px] p-2 flex ">
+				<div className="bg-[#f9f9f9] rounded-xl w-[150px] max-lg:w-[110px] p-2 flex items-center ">
 					<FontAwesomeIcon
 						icon={faMapMarkerAlt}
 						className="w-[20px] h-[20px]"
 					/>
-					<select onChange={onChangeHandler} className="w-[150px] bg-transparent max-lg:w-[110px]">
+					<select onChange={onChangeHandler} className="w-[150px] bg-transparent max-lg:w-[100px]">
 						<option disabled hidden selected value="">
 							
 							location
@@ -34,7 +34,7 @@ const SearchBar = () => {
 			)}
 			{value && (
 				<select
-					className="bg-[#f9f9f9] rounded-xl w-[150px] max-lg:w-[110px] p-2"
+					className="bg-[#f9f9f9] rounded-xl w-[150px] max-lg:w-[110px] p-2 max-lg:text-center"
 					value={value}
 					onChange={onChangeHandler}
 				>
