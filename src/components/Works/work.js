2fa47b5a8c289993e221d0928bbd7ede51346c1b
Georgeshayek <georgeshayek85@gmail.com>
2024-03-01 11:20:05 +0200

    css fixes second part

--- src/components/Works/work.js
+++ src/components/Works/work.js
@@ -9,10 +9,10 @@ const Work=()=>{
         <div className="bg-[#fbfbfb]">      
         <div className="container mx-auto">
         <div className="px-20 max-lg:px-8">
-    <div className=" lg:grid lg:grid-cols-3	 max-lg:flex-col max-lg:order1   py-6 items-center">
+    <div className=" grid grid-cols-3 py-6 items-center">
         <TextsWork/>
-        <div className="lg:col-span-1 lg:col-start-4 max-lg:grid-rows-3 max-lg:col-start-2 max-lg:mx-auto ">
-            <button className="py-4 px-12 lg:mt-[120px] border border-[#00cc99] text-[#00cc99] bg-transparent rounded  ">Learn More</button>
+        <div className="lg:col-span-1 lg:col-start-4 max-lg:row-start-3 max-lg:col-start-1 max-lg:col-span-3 max-lg:mx-auto ">
+            <button className="py-4 px-12 max-lg:py-3  lg:mt-[120px] border border-[#00cc99] text-[#00cc99] bg-transparent rounded  ">Learn More</button>
         </div>
         <Items/>
     </div>
