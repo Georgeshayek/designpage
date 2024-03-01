53c87cc9f3416d2df2abdaf22fb3084dfe398c95
Georgeshayek <georgeshayek85@gmail.com>
2024-03-01 15:19:08 +0200

    third part icon fixes

--- src/components/Popular/components/swiperItem.js
+++ src/components/Popular/components/swiperItem.js
@@ -3,7 +3,7 @@ const SwiperItem=(props)=>{
     return (
         
         <div className="group bg-[#ffffff] lg:p-4 max-lg:min-w-[120px] mr-4 max-lg:mt-4 min-h-[210px]  max-lg:min-h-[150px] rounded-xl relative hover:bg-[#00cc99]">
-            <Image src={props.imageurl} height={50} width={50} alt='image' className='py-2 max-lg:ml-1 group-hover:brightness-200	'/>
+            {props.imageurl}
             <h2 className="font-bold lg:text-[20px] max-lg:text-[12px]  py-2 pb-8 max-lg:pb-4 max-lg:ml-1 group-hover:text-white ">{props.title}</h2>
             <p className=" absolute text-[#8a8a8a] text-[8px] bottom-2 max-lg:ml-1 group-hover:text-white">{props.vacancy} Jobs Vacancy</p>
         </div>
--- src/components/Popular/components/swipercont.js
+++ src/components/Popular/components/swipercont.js
@@ -1,6 +1,12 @@
 "use client"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useMediaQuery } from "react-responsive";
+import { AiFillBank } from "react-icons/ai";
+import { LuBrain } from "react-icons/lu";
+import { ImStatsBars } from "react-icons/im";
+import { FaClockRotateLeft } from "react-icons/fa6";
+
+
 // Import Swiper styles
 import 'swiper/css';
 import SwiperItem from './swiperItem';
@@ -8,12 +14,12 @@ import SwiperItem from './swiperItem';
 const SwiperCont=()=>{
     const isMaxLg = useMediaQuery({ maxWidth: 1023 });
     const images = [
-		{ imageurl: "/brain.svg", title: "Design and Development", vacancy:350 },
-		{ imageurl: "/stats.svg", title: "Accounting and Finance", vacancy:300 },
-		{ imageurl: "/bank.svg", title: "Bank instituion", vacancy:250 },
-		{ imageurl: "/clipboard.svg", title: "Product management", vacancy:200 },
-		{ imageurl: "/pdf.svg", title: "Bank instituion", vacancy:350 },
-		{ imageurl: "/brain.svg", title: "Bank instituion", vacancy:150 },
+		{ imageurl: <LuBrain  className="text-[#00cc99] h-[50px] w-[50px] py-2 max-lg:ml-1 group-hover:text-white" /> , title: "Design and Development", vacancy:350 },
+		{ imageurl: <ImStatsBars className="text-[#00cc99] h-[50px] w-[50px] py-2 max-lg:ml-1 group-hover:text-white" /> , title: "Accounting and Finance", vacancy:300 },
+		{ imageurl: <AiFillBank className="text-[#00cc99] h-[50px] w-[50px] py-2 max-lg:ml-1 group-hover:text-white" /> , title: "Bank instituion", vacancy:250 },
+		{ imageurl: <FaClockRotateLeft className="text-[#00cc99] h-[50px] w-[50px] py-2 max-lg:ml-1 group-hover:text-white" /> , title: "Product management", vacancy:200 },
+		{ imageurl: <LuBrain className="text-[#00cc99] h-[50px] w-[50px] py-2 max-lg:ml-1 group-hover:text-white" /> , title: "Bank instituion", vacancy:350 },
+		{ imageurl: <AiFillBank className="text-[#00cc99] h-[50px] w-[50px] py-2 max-lg:ml-1 group-hover:text-white" /> , title: "Bank instituion", vacancy:150 },
 	];
 
     return(
